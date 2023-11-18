b9139c661d7c8e1c3031a9c28836ed6d2fbc281b
Adrian Camarena <adriancamarena2310mail.com>
2023-11-18 15:59:45 -0600

    video-88 Diseño de Gifs Components

---
+++ src/app/gifs/components/shearch-box/shearch-box.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gif-shearch-box',
+  template: `
+  <h5>Buscar</h5>
+  <input type="text"
+  class="form-control"
+  placeholder="Buscar gifs"
+  >
+  `,
+})
+export class ShearchBoxComponent {
+  }
+
+
--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home/home-Page.component';
+import { ShearchBoxComponent } from './components/shearch-box/shearch-box.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-
+    HomePageComponent,
+    ShearchBoxComponent
+  ],
+  exports: [
+    HomePageComponent
   ]
 })
 export class GifsModule { }
