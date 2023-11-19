c7b8e91e545002a6c8deceb7fb273e12966c6dd5
Adrian Camarena <adriancamarena2310mail.com>
2023-11-19 00:03:44 -0600

    video-108 Lazy Image - Parte 2

--- src/app/shared/components/lazyImage/lazyImage.component.ts
+++ src/app/shared/components/lazyImage/lazyImage.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazyImage',
   templateUrl: './lazyImage.component.html'
 })
-export class LazyImageComponent implements OnInit {
+export class LazyImageComponent implements OnInit{
 
   constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    if(!this.url)  throw new Error('Method not implemented.');
   }
 
+  public hasLoaded: boolean = false;
+
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = "";
+
+  onLoad(){
+    console.log("image loaded");
+    this.hasLoaded = true;
+  }
 }
