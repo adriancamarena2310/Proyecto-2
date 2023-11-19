8b00f3828aad30cb353333f87e5791577e483f23
Adrian Camarena <adriancamarena2310mail.com>
2023-11-18 19:46:38 -0600

    vdeo-92 Validaciones al servicio

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -13,8 +13,22 @@ get tagsHistory(){
   return [...this._tagHistory];
 }
 
+private organizeHistory (tag: string){
+  tag = tag.toLocaleLowerCase();
+
+  if( this._tagHistory.includes(tag)){
+    this._tagHistory = this._tagHistory.filter( (oldTag => oldTag !== tag));
+  }
+
+  this._tagHistory.unshift( tag );
+  this._tagHistory = this._tagHistory.splice(1, 10);
+}
 
 searchTag( Tag: string) : void{
+  if(this.tagsHistory.length){}
+
+  this.organizeHistory(Tag);
+
   this._tagHistory.unshift(Tag);
 }
 }
